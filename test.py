d0e590292ef295db3fc77838df124cda5322b605
Ida Nurjanova <ida@Idas-MacBook-Pro.local>
2025-03-07 18:29:34 -0500

    Tested the env for python coding

---
+++ test.py
@@ -0,0 +1,51 @@
+# print("Hello, Amazon SDE Prep!")
+
+"""
+name = "Umida"
+age = 33
+print(f"My name is {name} and I am {age} years old.")
+"""
+"""
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number"))
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1-num2}")
+print(f"Product: {num1 * num2}")
+print(f"Division: {num1 / num2}")
+"""
+"""
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:    
+    print("Even number")
+else: 
+    print("Odd number")
+"""
+"""
+# Loops: Print Numbers from 1 to 10
+for i in range (1,11):
+    print(i)
+"""
+"""
+# Functions: Calculate Factorial  -- write a function to find a factorial of a number
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i 
+    return result
+
+print(factorial(10))
+"""
+"""
+# FizzBuzz
+for i in range(1, 51):
+    if i % 3 == 0  and i % 5 == 0:
+        print ("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+ """
+ 
+   
+
